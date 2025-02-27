ee2475eb16e57f14dfb09174da9619cfd129cf54
tanadol097 <flerook@gmail.com>
2025-02-28 01:56:17 +0700

    icon0

---
+++ app/src/main/java/com/example/myproject/api/DocumentAPI.kt
@@ -0,0 +1,26 @@
+package com.example.myproject.api
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface DocumentAPI {
+    @GET("user-files/1") // ต้องตรงกับ API ของ Backend
+    suspend fun getDocuments(): List<DocumentItem>
+
+    companion object {
+        fun create(): DocumentAPI {
+            return Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:3000/") // ใช้ IP จำลอง Emulator
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(DocumentAPI::class.java)
+        }
+    }
+}
+
+data class DocumentItem(
+    val id: Int,
+    val document_url: String,
+    val uploaded_at: String
+)
