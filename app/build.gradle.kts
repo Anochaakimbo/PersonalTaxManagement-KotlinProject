e27f74112beefa6689ec59e77f859d0b3713df4b
Anochaakimbo <bbcorn123za@gmail.com>
2025-03-02 00:41:18 +0700

    test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.espresso.core)
     implementation(libs.cronet.embedded)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
